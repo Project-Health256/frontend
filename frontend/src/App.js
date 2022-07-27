0364b94100e7dcdcb3c6f41458506b34c01764df
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2022-07-26 22:15:44 -0400

    React Installation & Component Abstraction

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import Calculator from "./components/Calculator/Calculator";
+import FAQ from "./components/FAQ/FAQ";
+import SignUp from "./components/SignUp/SignUp";
+
+function App() {
+  return (
+    <div>
+      <SignUp />
+      <FAQ />
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
