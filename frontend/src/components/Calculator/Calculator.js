5b8a28b3b210c89425b23567553c4110a9941166
Matthew <tanmatt804@gmail.com>
2022-07-28 13:36:40 -0400

    changed metric label

--- frontend/src/components/Calculator/Calculator.js
+++ frontend/src/components/Calculator/Calculator.js
@@ -121,7 +121,7 @@ export default function Calculator() {
                   <option value="-20">Metric (kg, cm)</option>
                 </select>
                 <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-                  Company (Ex. Google)
+                  Units
                 </label>
               </div>
             </div>
