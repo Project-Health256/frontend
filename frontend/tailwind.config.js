3ab033c433269ec202a336387d6331ba9211d95c
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2022-07-29 14:02:33 -0400

    custom color #1 added to tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+const { blue } = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,6 +8,20 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      blueRYB: {
+        "50": "#ADADFF",
+        "100": "#9999FF",
+        "200": "#8585FF",
+        "300": "#7070FF",
+        "400": "#5C5CFF",
+        "500": "#4747FF",
+        "600": "#3333FF",
+        "700": "#1F1FFF",
+        "800": "#0A0AFF",
+        "900": "#0000F5",
+      }
+    }
   },
   plugins: [
     require('flowbite/plugin')
