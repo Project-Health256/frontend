2799ba237fb3f9a852fb9ebb9e317043cd47c880
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2022-07-29 21:15:53 -0400

    Outline Of Sign Up Page Complete

---
+++ frontend/src/Pages/SignUp/components/InputTextForm.js
@@ -0,0 +1,8 @@
+import React from "react";
+export function InputTextForm({ text }) {
+  return <div className="relative z-0 mb-6 w-8 group">
+    <input type="text" name="floating_email" className="block py-2.5 px-0 w-[50em] text-sm text-white bg-transparent border-0 border-b-2 border-white appearance-none dark:text-white dark:border-white dark:focus:border-white focus:outline-none focus:ring-0 focus:border-white peer" placeholder=" " required="" />
+
+    <label className="peer-focus:font-medium absolute text-sm text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{text}</label>
+  </div>;
+}
