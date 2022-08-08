4d53c1fe906aca2ceb6d16ec47da5185bd7fff21
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2022-08-08 13:26:08 -0400

    Charts are rendered on Calculator Pages

--- frontend/src/Pages/FAQ/components/FoodPlate.js
+++ frontend/src/Pages/FAQ/components/FoodPlate.js
@@ -1,33 +1,53 @@
 import React from "react";
-import { Bar } from "react-chartjs-2";
+import { Line, Bar } from "react-chartjs-2";
 import { useState } from "react";
 import { UserData } from "../../../Data/Data";
 import { Chart as ChartJS } from "chart.js/auto";
 
 export default function FoodPlate() {
-  const [userData, setUserData] = useState({
+  const [userGainData, setUserGainData] = useState({
     labels: UserData.map((data) => data.year),
     datasets: [
       {
         label: "Users Gained",
         data: UserData.map((data) => data.userGain),
         backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-          "#ff66cc",
+          "#fff"
+        ],
+        borderColor: "black",
+        borderWidth: 3,
+      },
+      {
+        label: "User Lost",
+        data: UserData.map(data => data.userLost),
+        backgroundColor: [
+          "#000"
         ],
         borderColor: "white",
-        borderWidth: 2,
-      },
+        borderWidth: 3,
+      }
     ],
   });
 
+  // const [userLostData, setUserLostData] = useState({
+  //   labels: UserData.map(data => data.year),
+  //   datasets: [
+  //     {
+  //       label: "User Lost",
+  //       data: UserData.map(data => data.userLost),
+  //       backgroundColor: [
+  //         "#000"
+  //       ],
+  //       borderColor: "white",
+  //       borderWidth: 2
+  //     }
+  //   ]
+  // })
+
   return (
-    <div className="ml-10 p-5 w-min">
-    <Bar data={userData} />
+    <div className="ml-10 p-5 w-3/4 flex justify-between">
+    <Line data={userGainData} />
+    {/* <Bar data={userLostData} /> */}
     </div>
   );
 }
