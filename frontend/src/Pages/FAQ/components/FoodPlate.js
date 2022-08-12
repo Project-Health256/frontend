15291f483cb3395a10b56bde97700d544641d5b5
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2022-08-11 23:16:16 -0400

    Charts render weight entries given by server

--- frontend/src/Pages/FAQ/components/FoodPlate.js
+++ frontend/src/Pages/FAQ/components/FoodPlate.js
@@ -1,33 +1,60 @@
 import React from "react";
 import { Line, Bar } from "react-chartjs-2";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { UserData } from "../../../Data/Data";
 import { Chart as ChartJS } from "chart.js/auto";
 
 export default function FoodPlate() {
+  const [user2WeightEntries, setUser2WeightEntries] = useState([]);
+
+  useEffect(() => {
+      fetch(`http://localhost:8000/weight-entries/2`)
+        .then(res => res.json())
+        .then(data => setUser2WeightEntries({
+          labels: data.map(data => data.created_at),
+          datasets: [
+            {
+              label: "Weight Entries",
+              data: data.map(data => data.weight_lbs),
+              backgroundColor:"#fff",
+              borderColor: "red",
+              borderWidth: 3,
+              color: "red",
+            },
+          ],
+        }))
+  }, [])
+
+console.log(user2WeightEntries)
+
+
+
   const [userGainData, setUserGainData] = useState({
-    labels: UserData.map((data) => data.year),
+    labels: UserData.map((data) => data.id),
     datasets: [
       {
-        label: "Users Gained",
-        data: UserData.map((data) => data.userGain),
+        label: `Week`,
+        data: UserData.map((data) => data.weeks),
         backgroundColor: [
           "#fff"
         ],
         borderColor: "black",
         borderWidth: 3,
+        color: "black",
       },
-      {
-        label: "User Lost",
-        data: UserData.map(data => data.userLost),
-        backgroundColor: [
-          "#000"
-        ],
-        borderColor: "white",
-        borderWidth: 3,
-      }
+      // {
+      //   label: "User Lost",
+      //   data: UserData.map(data => data.userLost),
+      //   backgroundColor: [
+      //     "#000"
+      //   ],
+      //   borderColor: "white",
+      //   borderWidth: 3,
+      // }
     ],
   });
+
+  // console.log(userGainData)
 
   // const [userLostData, setUserLostData] = useState({
   //   labels: UserData.map(data => data.year),
@@ -46,8 +73,7 @@ export default function FoodPlate() {
 
   return (
     <div className="ml-10 p-5 w-3/4 flex justify-between">
-    <Line data={userGainData} />
-    {/* <Bar data={userLostData} /> */}
+      <Line data={user2WeightEntries} />
     </div>
   );
 }
