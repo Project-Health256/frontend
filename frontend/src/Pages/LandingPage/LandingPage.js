24ce24bde1c930b2ed790074eebac9be4f8c36f5
Matt-Tan15 <tanmatt804@gmail.com>
2022-08-16 14:53:23 -0400

    implemented navbar

--- frontend/src/Pages/LandingPage/LandingPage.js
+++ frontend/src/Pages/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
 import { CalculatorCard } from "../Calculator/CalculatorCard";
 import { FAQCard } from "../FAQ/FAQCard";
 import { SignUpSection } from "../SignUp/SignUpSection";
-
+import NavBar from "../../global-components/NavBar/Navbar";
 export function LandingPage() {
   return (
     <>
+      <NavBar/>
+      <br></br>
       <SignUpSection />
       <div className="flex justify-evenly pt-8 bg-[#e89c30] pb-8">
         <CalculatorCard />
