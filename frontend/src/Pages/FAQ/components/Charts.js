4095463445ce3e8fcdd3f9d0ac76c34a4f7b6e65
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2022-08-18 11:54:27 -0400

    Modifed charts with localstorage implementaion

--- frontend/src/Pages/FAQ/components/Charts.js
+++ frontend/src/Pages/FAQ/components/Charts.js
@@ -1,18 +1,48 @@
-import React from "react";
-import { Line, Bar } from "react-chartjs-2";
-import { useState, useEffect } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { Line } from "react-chartjs-2";
 import { UserData } from "../../../Data/Data";
 import { Chart as ChartJS } from "chart.js/auto";
-import ToggleSwitch from "../../../global-components/LinkButton/ToggleSwitch";
-import RoundedInputNumberForm from "../../../global-components/RoundedInputNumberForm";
 
 export default function Charts() {
-  const [user2WeightEntries, setUser2WeightEntries] = useState([]);
+  const [userWeightEntries, setUserWeightEntries] = useState([]);
+  const user = JSON.parse(window.localStorage.getItem('currUser'))
+
+  function renderChart(data) {
+    if (!data.message) {
+      setUserWeightEntries({
+        labels: data.map(data => data.created_at),
+        datasets: [
+          {
+            label: "Losing Weight",
+            data: data.map(data => data.weight_lbs),
+            backgroundColor: "#fff",
+            borderColor: "red",
+            borderWidth: 3,
+            color: "red",
+          },
+        ],
+      })
+    } else {
+      setUserWeightEntries({
+        labels: ['empty'],
+        datasets: [
+          {
+            label: "Losing Weight",
+            data: [25, 50, 100, 150, 200, 250, 300],
+            backgroundColor: "#fff",
+            borderColor: "red",
+            borderWidth: 3,
+            color: "red",
+          },
+        ],
+      })
+    }
+  }
 
   useEffect(() => {
-    fetch(`http://localhost:8000/weight-entries/2`)
+    fetch(`http://localhost:8000/weight-entries/${user.id}`)
       .then(res => res.json())
-      .then(data => setUser2WeightEntries({
+      .then(data => setUserWeightEntries({
         labels: data.map(data => data.created_at),
         datasets: [
           {
@@ -25,7 +55,7 @@ export default function Charts() {
           },
         ],
       }))
-  }, [user2WeightEntries])
+  }, [userWeightEntries, user.id])
 
   const options = {
     plugins: {
@@ -36,10 +66,7 @@ export default function Charts() {
     }
   }
 
-  console.log(user2WeightEntries)
-
-
-// Data used to reset chart's in case charts no longer appear
+  // Data used to reset chart's in case charts no longer appear
   const [userGainData, setUserGainData] = useState({
     labels: UserData.map((data) => data.id),
     datasets: [
@@ -60,33 +87,39 @@ export default function Charts() {
     event.preventDefault();
 
     const updatedWeightEntries = Number(event.target.weightEntries.value);
-    // console.log(updatedWeightEntries)
+
     const newEntry = {
-      "weight_lbs" : updatedWeightEntries,
-      "userStartingMetrics" : 2
+      "weight_lbs": updatedWeightEntries,
+      "userStartingMetrics": user.id
     }
     fetch(`http://localhost:8000/weight-entries/`, {
-      method : "POST",
-      headers: { "Content-Type" : "application/json" },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newEntry)
     })
   }
 
   return (
     <div className="ml-10 p-5 w-3/4">
-      <Line data={user2WeightEntries} options={options} />
+      <div className="p-10 bg-blueRYB-900">
+      <button class="bg-transparent hover:bg-gray-500 text-white font-semibold hover:text-white py-2 px-4 border border-gray-500 hover:border-transparent rounded">
+  Button
+</button>
+      </div>
       <br />
       <br />
+      <Line data={userGainData} options={options} />
       <br />
-    <form onSubmit={UpdateUserEntries}>
-    <div class="w-1/2 md:w-1/3 px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for='weight-entries'>
-                Weight Entries
-            </label>
-            <input class="appearance-none block w-1/2 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id='weightEntries' type="float"  />
+      <br />
+      <form onSubmit={UpdateUserEntries}>
+        <div class="w-1/2 md:w-1/3 px-3 mb-6 md:mb-0">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for='weight-entries'>
+            Weight Entries
+          </label>
+          <input class="appearance-none block w-1/2 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id='weightEntries' type="float" />
         </div>
         <button className="border-purple-500" id="btn-submit">Update Chart</button>
-    </form>
+      </form>
     </div>
   );
 }
