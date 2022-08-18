e5ba40e1d800ef256381844054e190cfe29797a7
Matt-Tan15 <tanmatt804@gmail.com>
2022-08-18 15:20:38 -0400

    fixed color

--- frontend/src/Pages/LandingPage/LandingPage.js
+++ frontend/src/Pages/LandingPage/LandingPage.js
@@ -16,7 +16,7 @@ export function LandingPage() {
       <br></br>
       {!isAuth && !window.localStorage.getItem("token") && <SignUpSection />}
       {window.localStorage.getItem("token") && <AuthLandingPageTop />}
-      <div className="flex flex-col sm:flex-row items-center md:flex-row lg:flex-row sm:justify-evenly pt-8 bg-[#e89c30] pb-8">
+      <div className="flex flex-col sm:flex-row items-center md:flex-row lg:flex-row sm:justify-evenly pt-8 pb-8">
         <CalculatorCard />
         <FAQCard />
       </div>
