59d752c9e3b252ee03c8e09e85766ce18758f782
Matt-Tan15 <tanmatt804@gmail.com>
2022-08-18 15:48:10 -0400

    added select option for sessions

--- frontend/src/AuthPages/ChartPage/ChartPage.js
+++ frontend/src/AuthPages/ChartPage/ChartPage.js
@@ -3,20 +3,45 @@ import NavBar from "../../global-components/NavBar/Navbar";
 import Footer from "../../global-components/Footer/Footer";
 import { Calculator } from "../../Pages/Calculator/Calculator";
 import { UserProfileCard } from "./UserProfileCard";
+import { useState } from "react";
 
 export function ChartPage() {
-    const windowStorageUser = window.localStorage.getItem("currUser");
-    const verifiedUser = JSON.parse(windowStorageUser);
-    console.log(verifiedUser);
+  const windowStorageUser = window.localStorage.getItem("currUser");
+  const verifiedUser = JSON.parse(windowStorageUser);
+  const [selectedSession, setSelectedSession] = useState(0);
+  const [userSessions, setUserSessions] = useState([]);
+
   return (
     <>
       <NavBar />
       <div className="flex justify-evenly mt-5">
         <div className="w-1/4">
-          <UserProfileCard firstName={verifiedUser.firstName} lastName={verifiedUser.lastName}/>
+          <UserProfileCard
+            firstName={verifiedUser.firstName}
+            lastName={verifiedUser.lastName}
+          />
         </div>
         <div className="">
-            <Calculator />
+          <Calculator />
+          <div className="flex w-1/3">
+            <label
+              for="userMetrics"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+            >
+              Select an option
+            </label>
+            <select
+              onChange={(e) => setSelectedSession(e.target.value)}
+              id="userMetrics"
+              className="bg-gray-50 mb-5 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            >
+              <option selected="">Choose a session</option>
+              <option value="US">United States</option>
+              <option value="CA">Canada</option>
+              <option value="FR">France</option>
+              <option value="DE">Germany</option>
+            </select>
+          </div>
         </div>
       </div>
       <br></br>
--- frontend/src/Pages/Calculator/CalculatorResultsTable/CalculatorResults.js
+++ frontend/src/Pages/Calculator/CalculatorResultsTable/CalculatorResults.js
@@ -1,7 +1,7 @@
 export function CalculatorResults(obj) {
     console.log(obj.props.bmr)
   return (
-    <div className="overflow-x-auto relative">
+    <div className="overflow-x-auto relative rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr className="flex justify-center">
