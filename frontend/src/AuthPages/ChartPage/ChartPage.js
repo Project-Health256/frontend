7f881e20c4d91d2a9515d3cbd920cff02e830475
Matthew <tanmatt804@gmail.com>
2022-08-22 13:54:43 -0400

    changed post weight entry button color for consistency

--- frontend/src/AuthPages/ChartPage/ChartPage.js
+++ frontend/src/AuthPages/ChartPage/ChartPage.js
@@ -135,7 +135,7 @@ export function ChartPage() {
                   >
                     Weight (lbs)
                   </label>
-                  <button type="submit" class="text-white mt-5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                  <button type="submit" class="text-white mt-3 bg-[#7066e0] hover:bg-[#655cc9] focus:ring-4 focus:outline-none focus:ring-[#b7b2ef] font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-[#7066e0] dark:hover:bg-[#655cc9] dark:focus:ring-[#b7b2ef]">Submit</button>
                 </div>
               </form>
             </>
