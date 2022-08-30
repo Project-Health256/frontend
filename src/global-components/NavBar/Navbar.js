10afe474276697431a89edd49f05771af0cfd63c
Matt-Tan15 <tanmatt804@gmail.com>
2022-08-30 09:47:55 -0400

    changed providentLife logo link

--- src/global-components/NavBar/Navbar.js
+++ src/global-components/NavBar/Navbar.js
@@ -19,7 +19,7 @@ function NavBar() {
     <>
       <nav className="bg-white px-2 sm:px-4 py-1 dark:bg-gray-900 w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
         <div className="container flex flex-wrap justify-between items-center mx-auto">
-          <a href="http://localhost:3000" className="flex items-center">
+          <a href="/" className="flex items-center">
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white italic LI-UI ls">
               Provident Life
             </span>
